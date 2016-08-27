97d236e1be12734f5c6c56c28b41a0169ceb7119
electricmind <buy@neural.ru>
2016-08-27 09:58:09 +0300

    Introduce ComponentMap and ComponentFold

--- src/main/scala/com/github/dronegator/nlp/component/package.scala
+++ src/main/scala/com/github/dronegator/nlp/component/package.scala
@@ -10,40 +10,9 @@ package object component {
     def cfg: CFG
   }
 
-  trait ComponentMap[-A, +R] extends Component with Function1[A, R] {
-    def cfg: CFG
+  trait ComponentMap[-A, +R] extends Component with Function1[A, R]
 
-    def apply(in: A): R
-  }
-
-  trait ComponentFold[-A, R] extends Component with Function2[R, A, R]{
-    def cfg: CFG
-
-    //def apply(init:R, in: A): R
-
+  trait ComponentFold[-A, R] extends Component with Function2[R, A, R]   {
     def init: R
   }
-
-//  case class Map1() extends ComponentMap[Int, Int] {
-//    val cfg = ???
-//
-//  }
-//  val map = Map1()
-//
-//  List(1,2,3).map(map)
-//
-//  case class Fold() extends ComponentFold[String, Int] {
-//    val cfg = ???
-//
-//    override def init: String = ""
-//
-//    def apply(x: String, y: Int) =
-//      s"$x $y"
-//  }
-//
-//
-//  val fold = Fold()
-//
-//  List(1,2,3).foldLeft(fold.init)(fold)
-
 }
