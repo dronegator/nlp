c1531a72b580a90559f96487effeff693bc3f16c
dronegator <dronegator@gmail.com>
2016-09-20 20:53:35 +0300

    Factor our getSession

--- web/src/main/scala/com/github/dronegator/nlp/main/session/NLPTWebServiceSessionTrait.scala
+++ web/src/main/scala/com/github/dronegator/nlp/main/session/NLPTWebServiceSessionTrait.scala
@@ -1,8 +1,8 @@
 package com.github.dronegator.nlp.main.session
 
 /**
-  * Created by cray on 9/18/16.
-  */
+ * Created by cray on 9/18/16.
+ */
 
 import java.util.UUID
 
@@ -10,10 +10,9 @@ import akka.http.scaladsl.model.headers.HttpCookie
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import akka.util.Timeout
-import com.github.dronegator.nlp.main
-import com.github.dronegator.nlp.main.Concurent
 import com.github.dronegator.nlp.main.session.SessionManager.CreateSession
 import com.github.dronegator.nlp.main.session.SessionStorage.SessionMessage
+import com.github.dronegator.nlp.main.{Concurent, NLPTAppForWeb}
 import com.github.dronegator.nlp.utils.typeactor._
 
 import scala.concurrent.duration._
@@ -22,16 +21,48 @@ object NLPTWebServiceSessionTrait {
 
 }
 
-trait NLPTWebServiceSessionTrait
-  extends
-    main.NLPTAppForWeb {
-  this: Concurent =>
+protected trait SessionStorageTrait {
+  this: NLPTAppForWeb with Concurent =>
 
   lazy val propsSessionStorage = SessionStorage.props(cfg)
+
+  private implicit val timeout: Timeout = 10 seconds
+
+  def getSession(sessionId: SessionId) =
+    system.actorSelection(s"akka://default/user/$sessionId")
+      .resolveOne()
+      .map {
+        TypeActorRef[SessionMessage](_)
+      }
+      .recover {
+        case th: Throwable =>
+          system.actorOf(propsSessionStorage)
+      }
+      .map { typeActorRef =>
+        SessionManager.SessionRef(sessionId, typeActorRef)
+      }
+}
+
+protected trait SessionManagerTrait {
+  this: NLPTAppForWeb with Concurent with SessionStorageTrait =>
 
   lazy val propsSessionManager = SessionManager.props(cfg, propsSessionStorage)
 
   lazy val sessionManager = system.actorOf(propsSessionManager)
+
+  private implicit val timeout: Timeout = 10 seconds
+
+  override def getSession(sessionId: SessionId) =
+    (sessionManager ask CreateSession(SessionId(sessionId))).mapTo[SessionManager.SessionRef]
+
+}
+
+trait NLPTWebServiceSessionTrait
+  extends NLPTAppForWeb
+  with SessionStorageTrait
+  /*with SessionManagerTrait */ {
+  this: Concurent =>
+
   override lazy val route: Route =
     optionalCookie("sessionId") {
       case Some(cookie) =>
@@ -59,22 +90,4 @@ trait NLPTWebServiceSessionTrait
             }
         }
     }
-  private implicit val timeout: Timeout = 10 seconds
-
-  def getSession(sessionId: SessionId) =
-    system.actorSelection(s"akka://default/user/$sessionId")
-      .resolveOne()
-      .map {
-        TypeActorRef[SessionMessage](_)
-      }
-      .recover {
-        case th: Throwable =>
-          system.actorOf(propsSessionStorage)
-      }
-      .map { typeActorRef =>
-        SessionManager.SessionRef(sessionId, typeActorRef)
-      }
-
-  def getSessionFromManager(sessionId: String) =
-    sessionManager ask CreateSession(SessionId(sessionId))
 }
