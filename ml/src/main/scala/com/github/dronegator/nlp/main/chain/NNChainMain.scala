5c09677e2597b47c4eff29288a34767cc37b8bea
dronegator <dronegator@gmail.com>
2016-12-18 11:27:53 +0300

    Clone keyword network as chain network

---
+++ ml/src/main/scala/com/github/dronegator/nlp/main/chain/NNChainMain.scala
@@ -0,0 +1,276 @@
+package com.github.dronegator.nlp.main.chain
+
+import java.io.File
+
+import breeze.linalg.{DenseMatrix, DenseVector}
+import breeze.optimize.DiffFunction
+import com.github.dronegator.nlp.CaseClassToMap._
+import com.github.dronegator.nlp.component.tokenizer.Tokenizer.Token
+import com.github.dronegator.nlp.main.chain.NNSampleChainYesNo.Network
+import com.github.dronegator.nlp.main.{Concurent, MainConfig, _}
+import com.github.dronegator.nlp.trace._
+import com.github.dronegator.nlp.vocabulary.{Vocabulary, VocabularyImpl}
+import com.typesafe.config.ConfigFactory
+import com.typesafe.scalalogging.LazyLogging
+import configs.syntax._
+
+import scala.collection.JavaConverters._
+import scala.util.Random
+
+/**
+  * Created by cray on 12/15/16.
+  */
+
+trait NNChainFunctionConfig {
+  val nKlassen: Int
+  val nToken: Option[Int]
+  val dropout: Int
+  val winnerGetsAll: Boolean
+}
+
+trait NetworkBase {
+  val termToKlassen: DenseMatrix[Double]
+  val klassenToOut: DenseMatrix[Double]
+  val indexes: Seq[Int]
+}
+
+case class NNChainConfig(crossvalidationRatio: Int,
+                         doubleCrossvalidationRatio: Int,
+                         algorithm: Algorithm,
+                         regularization: Double,
+                         range: Double,
+                         maxIter: Int,
+                         rfo: Double,
+                         tolerance: Double,
+                         memoryLimit: Int,
+                         crossvalidationFrequency: Int,
+                         nKlassen: Int,
+                         nToken: Option[Int],
+                         dropout: Int,
+                         winnerGetsAll: Boolean,
+                         lambda: Double,
+                         delta: Double,
+                         eta: Double,
+                         stepSize: Double,
+                         minImprovementWindow: Token)
+  extends MLCfg
+    with NNChainFunctionConfig
+
+
+trait NNChainMain[N <: NetworkBase]
+  extends App
+    with MainTools
+    with MainConfig[NNChainConfig]
+    with NLPTAppMlTools[NNChainConfig, I, O, N]
+    with Concurent
+    with LazyLogging {
+
+  lazy val cfg: NNChainConfig = {
+    val cfg = ConfigFactory.parseMap(switches.asJava)
+      .withFallback(config.getConfig("chain"))
+      .extract[NNChainConfig].value
+
+    cfg.toMap.toList.sortBy(_._1).foreach {
+      case (key, value) =>
+        println(f"${key}%-32s = ${value}%-10s")
+    }
+
+    cfg
+  }
+
+
+  lazy val nToken = cfg.nToken.getOrElse(vocabulary.wordMap.keys.max)
+
+  lazy val vocabulary: Vocabulary = load(new File(fileIn)).time { t =>
+    logger.info(s"Vocabulary has loaded in time=$t")
+  } match {
+    case vocabulary: Vocabulary =>
+      vocabulary
+
+    case vocabulary =>
+      vocabulary: VocabularyImpl
+  }
+
+  lazy val (sense, senseCrossValidation) = vocabulary.sense.partition(_ => Random.nextInt(100) > cfg.doubleCrossvalidationRatio)
+
+  lazy val (auxiliary, auxiliaryCrossValidation) = vocabulary.auxiliary.partition(_ => Random.nextInt(100) > cfg.doubleCrossvalidationRatio)
+
+  private def proSampling(tokens: Set[Token]) =
+    vocabulary.nGram3.keysIterator
+      .filter {
+        case ws@w1 :: w2 :: w3 :: _ =>
+          ((tokens contains w2) || (tokens contains w2)) && ws.forall(_ <= nToken)
+      }
+
+  private def convert(sampling: Iterator[List[Token]]) =
+    sampling
+      .collect {
+        case ws@w1 :: w2 :: w3 :: _ =>
+          lazy val input = (w1, w3)
+
+          val output = DenseVector[Double](1)
+
+          if (vocabulary.sense contains w2)
+            Some(input -> (output := 1.0))
+          else if (vocabulary.auxiliary contains w2)
+            Some(input -> (output := 0.0))
+          else None
+      }
+      .collect {
+        case Some(sample) => sample
+      }
+      .toIterable
+
+
+  lazy val sampling: Iterable[((Token, Token), O)] =
+    convert(proSampling(sense ++ auxiliary))
+
+  lazy val samplingDoubleCross =
+    convert(proSampling(senseCrossValidation ++ auxiliaryCrossValidation))
+
+  override def printNetwork(network: N): Unit = {
+    println("== Classes: ")
+
+    for (n <- (0 until network.termToKlassen.rows)) {
+      val vector = network.termToKlassen(n, ::).t
+
+      println(s"====== $n ==")
+      val tokens = vector.toScalaVector()
+        .zipWithIndex
+        .sortBy(-_._1)
+        .map(x => x._2 -> x._1)
+
+      tokens.foreach {
+        case (token, weight) =>
+          println(f"${
+            vocabulary.wordMap(token)
+          }%10s $weight")
+
+      }
+    }
+
+  }
+
+  implicit val orderingDenseVectorDouble = Ordering.fromLessThan[DenseVector[Double]] {
+    (x, y) =>
+      (x :> y).forall(x => x)
+  }
+
+  def net(network: N): NN[(Token, Token), DenseVector[Double], _, N]
+
+  override def calc(sampling: Iterable[((Token, Token), O)]): Unit = {
+    val calulate = net(nn.network(network))
+
+    val map = sampling
+      .foldLeft(Map[Int, (Double, DenseVector[Double])]()) {
+        case (map, ((t1, t3), _)) =>
+          val vector = calulate.forward(nn.network(network), (t1, t3))
+
+          vocabulary.map2ToMiddle(t1 :: t3 :: Nil)
+            .foldLeft(map) {
+              case (map, (weight, t2)) =>
+                map + (map.get(t2) match {
+                  case Some((accumulate, outcome)) =>
+                    t2 -> (accumulate + 1.0, outcome + vector)
+                  case None =>
+                    t2 -> (1.0, vector)
+                })
+            }
+      }
+      .map {
+        case (token, (weight, estimation)) =>
+          (token, estimation :/ weight)
+      }
+
+    map.toSeq
+      .sortBy(_._2)
+      .foreach {
+        case (token, estimation) =>
+          println(
+            f"""${
+              vocabulary.wordMap.getOrElse(token, "***")
+            }%-20s ${estimation}""")
+      }
+
+    val senseErr = vocabulary.sense.flatMap(map.get).reduce(_ + _) :/ vocabulary.sense.size.toDouble
+
+    val auxiliaryErr = vocabulary.auxiliary.flatMap(map.get).reduce(_ + _) :/ vocabulary.auxiliary.size.toDouble
+
+    println(s"senseErr = $senseErr")
+
+    println(s"auxiliaryErr = $auxiliaryErr")
+  }
+
+
+}
+
+object NNChainMain
+  extends NNChainMain[Network] {
+  override def nn: NNSampleTrait[(Token, Token), O, Network, _, _] with DiffFunction[DenseVector[Double]] =
+    new NNSampleChainYesNo(
+      nKlassen = cfg.nKlassen,
+      nToken = nToken,
+      dropout = cfg.dropout,
+      winnerGetsAll = cfg.winnerGetsAll,
+      sampling = sampling)
+
+  def net(network: Network): NN[(Token, Token), DenseVector[Double], _, Network] =
+    new NNChainYesNoImpl(network, cfg.nKlassen)
+
+  try {
+    report
+  } finally {
+    system.terminate()
+    mat.shutdown()
+  }
+
+  override def nnCross: DiffFunction[DenseVector[Double]] =
+    new NNSampleChainYesNo(nKlassen = cfg.nKlassen,
+      nToken = nToken,
+      dropout = 0,
+      winnerGetsAll = false,
+      sampling = samplingCross)
+
+  override def nnDoubleCross: DiffFunction[DenseVector[Double]] =
+    new NNSampleChainYesNo(nKlassen = cfg.nKlassen,
+      nToken = nToken,
+      dropout = 0,
+      winnerGetsAll = false,
+      sampling = samplingDoubleCross)
+
+}
+
+object NNChainMainWithConst
+  extends NNChainMain[NNSampleChainYesNoWithConst.Network] {
+  override def nn: NNSampleTrait[(Token, Token), O, NNSampleChainYesNoWithConst.Network, _, _] with DiffFunction[DenseVector[Double]] =
+    new NNSampleChainYesNoWithConst(
+      nKlassen = cfg.nKlassen,
+      nToken = nToken,
+      dropout = cfg.dropout,
+      winnerGetsAll = cfg.winnerGetsAll,
+      sampling = sampling)
+
+  def net(network: NNSampleChainYesNoWithConst.Network): NN[(Token, Token), DenseVector[Double], _, NNSampleChainYesNoWithConst.Network] =
+    new NNChainYesNoImplWithConst(network, cfg.nKlassen)
+
+  override def nnCross: DiffFunction[DenseVector[Double]] =
+    new NNSampleChainYesNoWithConst(nKlassen = cfg.nKlassen,
+      nToken = nToken,
+      dropout = 0,
+      winnerGetsAll = false,
+      sampling = samplingCross)
+
+  override def nnDoubleCross: DiffFunction[DenseVector[Double]] =
+    new NNSampleChainYesNoWithConst(nKlassen = cfg.nKlassen,
+      nToken = nToken,
+      dropout = 0,
+      winnerGetsAll = false,
+      sampling = samplingDoubleCross)
+
+  try {
+    report
+  } finally {
+    system.terminate()
+    mat.shutdown()
+  }
+}
---
+++ ml/src/main/scala/com/github/dronegator/nlp/main/chain/NNSampleChainYesNoWithConst.scala
@@ -0,0 +1,155 @@
+package com.github.dronegator.nlp.main.chain
+
+import breeze.linalg.{DenseMatrix, DenseVector}
+import breeze.numerics._
+import breeze.optimize.DiffFunction
+import com.github.dronegator.nlp.component.tokenizer.Tokenizer.Token
+import com.github.dronegator.nlp.main.chain.NNSampleChainYesNoWithConst.{Hidden, Network, Quality}
+import com.github.dronegator.nlp.main.{NN, NNCalcDenseVector, NNQuality, NNSampleTrait}
+
+import scala.util.Random
+
+/**
+  * Created by cray on 12/15/16.
+  */
+
+object NNSampleChainYesNoWithConst {
+
+  case class Quality(yes: Int, no: Int)
+
+  case class Network(constToKlassen: DenseVector[Double], termToKlassen: DenseMatrix[Double], klassenToOut: DenseMatrix[Double], indexes: Seq[Int])
+    extends NetworkBase
+
+  case class Hidden(klassenI: DenseVector[Double],
+                    klassenO: DenseVector[Double],
+                    outI: DenseVector[Double],
+                    outO: DenseVector[Double])
+
+}
+
+trait NNChainYesNoWithConst
+  extends NN[(Token, Token), DenseVector[Double], Hidden, Network] {
+
+  def winnerGetsAll: Boolean
+
+  override def forward(network: Network, hidden: Hidden, input: (Token, Token)): O =
+    input match {
+      case (in1, in2) =>
+        hidden.klassenI(0 until nKlassen) := network.termToKlassen(::, in1) * 1.0
+
+        hidden.klassenI(0 until nKlassen) :+= network.constToKlassen * 1.0
+
+        hidden.klassenI(nKlassen until nKlassen * 2) := network.termToKlassen(::, in2) * 1.0
+
+        hidden.klassenI(nKlassen until nKlassen * 2) :+= network.constToKlassen * 1.0
+
+        hidden.klassenO := hidden.klassenI.map(x => 1 / (1 + exp(-x)))
+
+        for (i <- network.indexes) {
+          hidden.klassenO(i) = 0.0
+        }
+
+        if (winnerGetsAll) {
+          val (i1x, v1x) = hidden.klassenO(0 until nKlassen).iterator.maxBy(_._2)
+
+          val (i2x, v2x) = hidden.klassenO(nKlassen until nKlassen * 2).iterator.maxBy(_._2)
+
+          val (i1n, v1n) = hidden.klassenO(0 until nKlassen).iterator.minBy(_._2)
+
+          val (i2n, v2n) = hidden.klassenO(nKlassen until nKlassen * 2).iterator.minBy(_._2)
+
+          hidden.klassenO := 0.0
+
+          hidden.klassenO(i1x) = v1n
+
+          hidden.klassenO(i2x + nKlassen) = v2n
+
+          hidden.klassenO(i1n) = v1n
+
+          hidden.klassenO(i2n + nKlassen) = v2n
+        }
+
+        hidden.outI := network.klassenToOut * hidden.klassenO
+
+        hidden.outO := hidden.outI.map(x => 1 / (1 + exp(-x)))
+    }
+
+  val nKlassen: Int
+
+  override def hiddenInit(): Hidden =
+    Hidden(
+      klassenI = DenseVector.zeros[Double](2 * nKlassen),
+      klassenO = DenseVector.zeros[Double](2 * nKlassen),
+      outI = DenseVector.zeros[Double](1),
+      outO = DenseVector.zeros[Double](1)
+    )
+}
+
+case class NNChainYesNoImplWithConst(network: Network, nKlassen: Int)
+  extends NNChainYesNoWithConst {
+  require(network.termToKlassen.rows == nKlassen)
+
+  val winnerGetsAll = false
+
+  def apply(input: (Token, Token)) =
+    forward(network, input)
+}
+
+class NNSampleChainYesNoWithConst(val nKlassen: Int,
+                                  nToken: Int,
+                                  dropout: Int,
+                                  val winnerGetsAll: Boolean,
+                                  val sampling: Iterable[((Token, Token), DenseVector[Double])])
+  extends NN[(Token, Token), DenseVector[Double], Hidden, Network]
+    with DiffFunction[DenseVector[Double]]
+    with NNSampleTrait[(Token, Token), DenseVector[Double], Network, Hidden, Quality]
+    with NNQuality[DenseVector[Double], Quality]
+    with NNCalcDenseVector
+    with NNChainYesNoWithConst {
+
+  override def network(vector: DenseVector[Double]): Network =
+    Network(
+      constToKlassen = vector(nToken * nKlassen + nKlassen * 2 until nToken * nKlassen + nKlassen * 2 + nKlassen),
+      termToKlassen = vector(0 until nToken * nKlassen).asDenseMatrix.reshape(nKlassen, nToken),
+      klassenToOut = vector(nToken * nKlassen until (nToken * nKlassen + nKlassen * 2)).asDenseMatrix.reshape(1, nKlassen * 2),
+      (0 until nKlassen * 2)
+        .filter(_ => Random.nextInt(100) < dropout)
+    )
+
+  override def size: Token = nKlassen * nToken + nKlassen * 2 + nKlassen
+
+  override def backward(nn: Network, gradient: Network, hidden: Hidden, input: (Token, Token), output: DenseVector[Double], result: DenseVector[Double]): Unit =
+
+    input match {
+      case (in1, in2) =>
+
+        val Network(gConstToKlassen, gTermToKlassen, gKlassen2Out, _) = gradient
+
+        val backOutI = (result - output) :* (result :* (-result + 1.0))
+
+        gKlassen2Out :+= (backOutI * hidden.klassenO.t)
+
+        val backKlassenO = nn.klassenToOut.t * backOutI
+
+        val backKlassenI = backKlassenO :* hidden.klassenO :* (-hidden.klassenO + 1.0)
+
+        gConstToKlassen :+= (backKlassenI(0 until nKlassen) * 1.0)
+
+        gConstToKlassen :+= (backKlassenI(nKlassen until 2 * nKlassen) * 1.0)
+
+        gTermToKlassen(::, in1) :+= (backKlassenI(0 until nKlassen) * 1.0)
+
+        gTermToKlassen(::, in2) :+= (backKlassenI(nKlassen until 2 * nKlassen) * 1.0)
+    }
+
+
+  override def quality(quality: Quality, result: DenseVector[Double]): Quality = {
+    Quality(
+      yes = if (result.exists(_ > 0.9)) quality.yes + 1 else quality.yes,
+      no = if (result.exists(_ < 0.1)) quality.no + 1 else quality.no)
+  }
+
+  override def quality: Quality =
+    Quality(0, 0)
+
+}
