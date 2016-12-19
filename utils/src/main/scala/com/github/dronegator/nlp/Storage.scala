b7bad068f066309aa81f23aaf1f8d8e935d91aed
dronegator <dronegator@gmail.com>
2016-12-20 00:09:37 +0300

    Add createApprox

--- utils/src/main/scala/com/github/dronegator/nlp/Storage.scala
+++ utils/src/main/scala/com/github/dronegator/nlp/Storage.scala
@@ -31,8 +31,48 @@ object Storage {
     }
 }
 
+object Conv {
+  def apply[PA, A](implicit conv: Conv[PA, A]) =
+    conv
+
+  def instance[PA, A](f: PA => A) =
+    new Conv[PA, A] {
+      override def conv(pa: PA): A =
+        f(pa)
+    }
+
+  implicit def pa2a[PA, A, PR <: HList, R <: HList](implicit genPA: Generic.Aux[PA, PR], genA: Generic.Aux[A, R], conv: Conv[PR, R]) =
+    instance[PA, A] { (pa: PA) =>
+      genA.from(conv.conv(genPA.to(pa)))
+    }
+
+
+  implicit def pr2r[PRH, PRT <: HList, PT <: HList](implicit conv: Conv[PRT, PT]) =
+    instance[PRH :: PRT, PRH :: PT] { (pr: PRH :: PRT) =>
+      pr.head :: conv.conv(pr.tail)
+    }
+
+  implicit def hnil2hnil =
+    instance[HNil, HNil] { (hnil: HNil) =>
+      hnil
+    }
+
+  implicit def pr2rSkip[PRH, PRT <: HList, PT <: HList](implicit conv: Conv[PRT, PT]) =
+    instance[PRH :: PRT, PT] { (pr: PRH :: PRT) =>
+      conv.conv(pr.tail)
+    }
+}
+
+trait Conv[PA, A] {
+  def conv(pa: PA): A
+}
+
+
 trait Storage[AI, A, B] {
   def create(id: AI, data: A): B
+
+  def createApprox[PA](id: AI, data: PA)(implicit conv: Conv[PA, A]): B =
+    create(id, conv.conv(data))
 
   def update(b: B): B
 
@@ -54,12 +94,27 @@ object StorageApp
 
   case class S(id: String @@ S, s1: String, s2: Int, t: T)
 
+  case class SData(s1: String, d: Double, s2: String, i: Int, t: T)
+
   println(3)
+
   val t = Storage[(Int, Int), TR].create(tag[TR]("qq"), (1, 2))
 
   val s = Storage[(String, Int, T), S].create(tag[S]("qq"), ("", 2, t))
 
   val s1 = Storage[(String, Int, T), S].create(tag[S]("qq"), ("", 2, TId(tag[TR]("tid"))))
 
+  //val a = the[Conv[(String, Int, T), SData]]
+
+  val s3 = Storage[(String, Int, T), S].createApprox(tag[S]("qq"), SData("", 2.0, "asdasd", 3, TId(tag[TR]("tid")): T))
+
+  case class V(i: Int, s: String)
+
+  case class W(i: Int, d: Double, s: String)
+
+  println(Conv[W, V].conv(W(1, 0.1, "1")))
+
   println(t, s, s1)
+
+  println(s3)
 }
