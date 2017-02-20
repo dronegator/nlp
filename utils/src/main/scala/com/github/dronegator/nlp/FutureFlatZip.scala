338e97af358076cac2e43a37ff9b9958be5e43be
dronegator <dronegator@gmail.com>
2017-02-20 22:28:21 +0300

    ReImplement FutureFlatZip

--- utils/src/main/scala/com/github/dronegator/nlp/FutureFlatZip.scala
+++ utils/src/main/scala/com/github/dronegator/nlp/FutureFlatZip.scala
@@ -4,9 +4,66 @@ package com.github.dronegator.nlp
   * Created by cray on 2/18/17.
   */
 
-import shapeless._
-import shapeless.ops.hlist._
+import shapeless.ops.tuple._
+
+import scala.annotation.implicitNotFound
 import scala.concurrent._
+
+object IsTuple {
+  def apply[A](implicit isTuple: IsTuple[A]) =
+    isTuple
+
+  def createIsTuple[A] =
+    new IsTuple[A] {}
+
+  implicit def isTuple1[A] =
+    createIsTuple[Tuple1[A]]
+
+  implicit def isTuple2[A, B] =
+    createIsTuple[Tuple2[A, B]]
+
+  implicit def isTuple3[A, B, C] =
+    createIsTuple[Tuple3[A, B, C]]
+
+  implicit def isTuple4[A, B, C, D] =
+    createIsTuple[Tuple4[A, B, C, D]]
+
+  implicit def isTuple5[A, B, C, D, E] =
+    createIsTuple[Tuple5[A, B, C, D, E]]
+}
+
+trait IsTuple[A] {
+
+}
+
+sealed trait Existence
+
+trait Exists extends Existence
+
+trait NotExists extends Existence
+
+trait IsTypeClassExists[TypeClass, Answer]
+
+object IsTypeClassExists {
+  private val evidence: IsTypeClassExists[Any, Any] =
+    new Object with IsTypeClassExists[Any, Any]
+
+  implicit def typeClassExistsEv[TypeClass, Answer](implicit a: TypeClass) =
+    evidence.asInstanceOf[IsTypeClassExists[TypeClass, Exists]]
+
+  implicit def typeClassNotExistsEv[TypeClass, Answer] =
+    evidence.asInstanceOf[IsTypeClassExists[TypeClass, NotExists]]
+}
+
+@implicitNotFound("Argument does not satisfy constraints: Not ${T}")
+trait Not[T]
+
+object Not {
+  private val evidence: Not[Any] = new Object with Not[Any]
+
+  implicit def notEv[T, Answer](implicit a: IsTypeClassExists[T, Answer], ne: Answer =:= NotExists) =
+    evidence.asInstanceOf[Not[T]]
+}
 
 trait FutureFlatZipLowest {
 
@@ -16,60 +73,64 @@ trait FutureFlatZipLowest {
   def createFutureFlatZip[A, B, C](f: (Future[A], Future[B], ExecutionContext) => Future[C]) =
     new FutureFlatZip[A, B] {
       type Out = C
+
       override def futureFlatZip(as: Future[A], bs: Future[B])(implicit ec: ExecutionContext): Future[C] =
         f(as, bs, ec)
     }
 
   implicit def futureFlatZipAnyAny[A, B](implicit
-                                         aNotAProduct: A <:!< Product,
-                                         bNotAProduct: B <:!< Product) =
+                                         aIsNotATuple: Not[IsTuple[A]],
+                                         bIsNotATuple: Not[IsTuple[B]]
+                                        ) =
     createFutureFlatZip[A, B, (A, B)] { (as, bs, ec) =>
       implicit val iec = ec
       as.zip(bs)
     }
 
-  implicit def futureFlatZipAnyTuple[A, B, BRepl <: HList, C](implicit
-                                                              aNotAProduct: A <:!< Product,
-                                                              genB: Generic.Aux[B, BRepl],
-                                                              tupler: Tupler.Aux[A :: BRepl, C]) =
+}
+
+trait FutureFlatZipLow
+  extends FutureFlatZipLowest {
+  implicit def futureFlatZipTupleAny[A, B, C](implicit
+                                              aIsNotATuple: Not[IsTuple[A]],
+                                              isTupleB: IsTuple[B],
+                                              prepend: Prepend.Aux[Tuple1[A], B, C]) =
     createFutureFlatZip[A, B, C] { (as, bs, ec) =>
       implicit val iec = ec
       as.zip(bs).map {
         case (a, b) =>
-          tupler(a :: genB.to(b))
+          prepend(Tuple1(a), b)
       }
     }
 
-  implicit def futureFlatZipTupleAny[A, ARepl <: HList, B, C, CRepl <: HList](implicit
-                                                                              bNotAProduct: B <:!< Product,
-                                                                              genA: Generic.Aux[A, ARepl],
-                                                                              prepend: Prepend.Aux[ARepl, B :: HNil, CRepl],
-                                                                              tupler: Tupler.Aux[CRepl, C]) =
+  implicit def futureFlatZipAnyTuple[A, B, C](implicit
+                                              isTupleA: IsTuple[A],
+                                              bIsNotATuple: Not[IsTuple[B]],
+                                              prepend: Prepend.Aux[A, Tuple1[B], C]) =
     createFutureFlatZip[A, B, C] { (as, bs, ec) =>
       implicit val iec = ec
       as.zip(bs).map {
         case (a, b) =>
-          tupler(genA.to(a) :+ b)
-      }
-    }
-
-  implicit def futureFlatZipTupleTuple[A, ARepl <: HList, B, BRepl <: HList, C, CRepl <: HList](implicit
-                                                                                                genA: Generic.Aux[A, ARepl],
-                                                                                                genB: Generic.Aux[B, BRepl],
-                                                                                                concat: Prepend.Aux[ARepl, BRepl, CRepl],
-                                                                                                tupler: Tupler.Aux[CRepl, C]) =
-    createFutureFlatZip[A, B, C] { (as, bs, ec) =>
-      implicit val iec = ec
-      as.zip(bs).map {
-        case (a, b) =>
-          tupler(genA.to(a) ++ genB.to(b))
+          prepend(a, Tuple1(b))
       }
     }
 
 }
 
 trait FutureFlatZipHigh
-  extends FutureFlatZipLowest {
+  extends FutureFlatZipLow {
+  implicit def futureFlatZipTupleTuple[A, B, C](implicit
+                                                isTupleA: IsTuple[A],
+                                                isTupleB: IsTuple[B],
+                                                concat: Prepend.Aux[A, B, C]) =
+    createFutureFlatZip[A, B, C] { (as, bs, ec) =>
+      implicit val iec = ec
+      as.zip(bs).map {
+        case (a, b) =>
+          concat(a, b)
+      }
+    }
+
 }
 
 
@@ -96,27 +157,74 @@ trait FutureFlatZip[A, B] {
   def futureFlatZip(as: Future[A], b: Future[B])(implicit ec: ExecutionContext): Future[Out]
 }
 
-object Example {
 
+object Example {
+  //import com.github.dronegator.nlp.FutureFlatZip
   import FutureFlatZip.FutureFlatZipOps
 
   import scala.concurrent.ExecutionContext.Implicits.global
+  import scala.concurrent._
+  import scala.concurrent.duration.Duration
 
-  val qq = the[FutureFlatZip[Int, (Int, Int)]]
+  def await[A](future: Future[A]) =
+    Await.result(future, Duration.Inf)
 
-  qq.futureFlatZip(Future(1), Future((2, 3))).map {
-    case (x, y, z) =>
-      x + y + z
-  }
+  await(Future(1).flatZip(Future(2))
+    .map {
+      case (x, y) =>
+        x + y
+    })
 
-  Future(1).flatZip(Future(2))
+  await(Future(1).flatZip(Future(2).flatZip(Future(3)))
+    .map {
+      case (x, y, z) =>
+        x + y + z
+    })
 
-  Future(1).flatZip(Future((2, 3))).map {
-    case (x, y, z) =>
-      x + y + z
-  }
-  Future((1, 2)).flatZip(Future((3, 4)))
-  Future(1).flatZip(Future(2).flatZip(Future(3)))
+  await(Future(1).flatZip((Future(2).flatZip(Future(3))))
+    .map {
+      case (x, y, z) =>
+        x + y + z
+    })
 
-  Future(1).flatZip(Future(2)).flatZip(Future(3)).flatZip(Future(4)).flatZip(Future((5, 6, 7, 8)))
+  await(Future(1).flatZip(Future(2)).flatZip((Future(3).flatZip(Future(4))))
+    .map {
+      case (x, y, z, z1) =>
+        x + y + z + z1
+    })
+
+
+  val a1 = await(Future(Seq(1)).flatZip(Future(Seq(2))).flatZip(Future(Seq(2))).flatZip(Future(Seq(4)))
+    .map {
+      case (a, b, c, d) =>
+        a ++ b ++ c ++ d
+    })
+
+  println(a1)
+
 }
+
+//object Example {
+//
+//  import FutureFlatZip.FutureFlatZipOps
+//
+//  import scala.concurrent.ExecutionContext.Implicits.global
+//
+//  val qq = the[FutureFlatZip[Int, (Int, Int)]]
+//
+//  qq.futureFlatZip(Future(1), Future((2, 3))).map {
+//    case (x, y, z) =>
+//      x + y + z
+//  }
+//
+//  Future(1).flatZip(Future(2))
+//
+//  Future(1).flatZip(Future((2, 3))).map {
+//    case (x, y, z) =>
+//      x + y + z
+//  }
+//  Future((1, 2)).flatZip(Future((3, 4)))
+//  Future(1).flatZip(Future(2).flatZip(Future(3)))
+//
+//  Future(1).flatZip(Future(2)).flatZip(Future(3)).flatZip(Future(4)).flatZip(Future((5, 6, 7, 8)))
+//}
